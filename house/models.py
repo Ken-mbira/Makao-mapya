35e9fea11d6b3884a8e2ed42471a635bbe182bd4
ken <ken.mbira@student.moringaschool.com>
2021-10-28 08:16:12 +0300

    create a houser application and the models involving a house

---
+++ house/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+from account.models import Account
+
+# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+
+class Country(models.Model):
+    name = models.CharField(max_length=200)
+
+class City(models.Model):
+    country = models.ForeignKey(Country,on_delete=models.RESTRICT)
+    name = models.CharField(max_length=200)
+
+class House(models.Model):
+    owner = models.ForeignKey(Account,on_delete=models.CASCADE)
+    name = models.CharField(max_length=200,unique=True)
+    description = models.TextField()
+    location = models.ForeignKey(City,on_delete=models.RESTRICT)
+    type = models.ForeignKey(Category,on_delete=models.RESTRICT)
+    date_added = models.DateTimeField(auto_now_add=True)
+    is_available = models.BooleanField(default=True)
+    bedrooms = models.IntegerField()
+    bathrooms = models.IntegerField()
+    garage = models.BooleanField()
